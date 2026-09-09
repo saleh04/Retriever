f71edfef99ddafe84c06e185844b7bca2137f1f7
saleh04 <saleh04mo@gmail.com>
2026-09-10 02:31:38 +0300

    Refactor construct_prompt method in CohereProvider and OpenAIProvider to use raw prompt content

--- src/stores/llm/providers/CohereProvider.py
+++ src/stores/llm/providers/CohereProvider.py
@@ -129,5 +129,5 @@ class CohereProvider(LLMInterface):
     def construct_prompt(self, prompt:str, role:str):
         return {
             "role" : role,
-            "content" : self.process_text(text=prompt)
+            "content" : prompt
         }
