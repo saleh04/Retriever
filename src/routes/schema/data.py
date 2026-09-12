58be44bceb91b513b0720cbffda4e4b87ad07869
saleh04 <saleh04mo@gmail.com>
2026-09-12 22:41:47 +0300

    Update file_id type in ProcessRequest to allow None value

--- src/routes/schema/data.py
+++ src/routes/schema/data.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 
 class ProcessRequest(BaseModel):
-    file_id: str = None
+    file_id: str | None = None
     chunk_size: int | None = 100
     overlap: int | None = 20
     do_reset: bool = False
